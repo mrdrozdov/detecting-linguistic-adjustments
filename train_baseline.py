c84600299d3a4aa3c635e6dea0df6e2a30c37415
Andrew Drozdov <andrew@mrdrozdov.com>
2019-06-30 14:19:28 -0400

    CUDA support.

--- train_baseline.py
+++ train_baseline.py
@@ -14,29 +14,34 @@ from read_data import *
 
 
 class BatchManager(object):
-    def __init__(self, pad_token=None, emb_layer=None):
+    def __init__(self, pad_token=None, emb_layer=None, cuda=False):
         super(BatchManager, self).__init__()
         self.pad_token = pad_token
         self.emb_layer = emb_layer
+        self.cuda = cuda
+
+    @property
+    def device(self):
+        return torch.cuda.current_device() if self.cuda else None
 
     def __tokenize_batch(self, sentences):
         return [self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize(' '.join(x)))
                 for x in sentences]
 
     def __pad_and_make_tensor(self, tokenized):
-        lengths = [len(x) for x in tokenized]
-        tensors = [self.emb_layer(torch.tensor(x, dtype=torch.long)) for x in tokenized]
+        lengths = torch.tensor([len(x) for x in tokenized], dtype=torch.long, device=self.device)
+        tensors = [self.emb_layer(torch.tensor(x, dtype=torch.long, device=self.device)) for x in tokenized]
         padded = torch.nn.utils.rnn.pad_sequence(tensors,
             batch_first=True, padding_value=self.pad_token)
         return torch.nn.utils.rnn.pack_padded_sequence(padded,
             batch_first=True, lengths=lengths, enforce_sorted=False)
-        
+
     def prepare_tokens(self, batch_map):
         tensor = self.__pad_and_make_tensor(batch_map['sentences'])
         return tensor
 
     def prepare_labels(self, batch_map):
-        return torch.tensor(batch_map['labels'], dtype=torch.long)
+        return torch.tensor(batch_map['labels'], dtype=torch.long, device=self.device)
 
 
 class Classifier(nn.Module):
@@ -68,7 +73,9 @@ def run(options):
     emb_layer = nn.Embedding.from_pretrained(torch.from_numpy(embeddings), freeze=True)
     batch_manager = BatchManager(
         pad_token=train_word2idx[AdjustmentDatasetBaseline.PADDING_TOKEN],
-        emb_layer=emb_layer)
+        emb_layer=emb_layer,
+        cuda=options.cuda)
+    batch_manager.emb_layer = emb_layer.to(batch_manager.device)
 
     model = Classifier(embedding_size=embeddings.shape[1])
     optimizer = optim.Adam(model.parameters(), lr=2e-3, betas=(0.9, 0.999), eps=1e-8)
@@ -77,14 +84,20 @@ def run(options):
     history['loss'] = []
     trail = 100
 
+    summary_every = 100
+    summary = []
+
+    if options.cuda:
+        model.cuda()
+
+    step = 0
+
     for epoch in range(num_epochs):
         for batch_map in train_iterator.get_iterator(batch_size=options.batch_size):
 
             packed_sequence = batch_manager.prepare_tokens(batch_map)
             labels_tensor = batch_manager.prepare_labels(batch_map)
-
             logits = model(packed_sequence)
-
             loss = nn.CrossEntropyLoss()(logits, labels_tensor)
 
             # Compute gradient.
@@ -107,8 +120,11 @@ def run(options):
                 history[k].append(batch_output[k])
                 history[k] = history[k][-trail:]
 
-            print('loss = {:.3f}, accuracy-mean = {:.3f}'.format(
+            print('step = {:08}, loss = {:.3f}, accuracy-mean = {:.3f}'.format(
+                step,
                 np.mean(history['loss']), np.mean(history['accuracy'])))
+
+            step += 1
 
 
 if __name__ == '__main__':
@@ -116,6 +132,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', default=None, type=int)
+    parser.add_argument('--cuda', action='store_true')
     parser.add_argument('--train_file', default='./adjustment-data.jsonl', type=str)
     parser.add_argument('--batch_size', default=4, type=int)
     options = parser.parse_args()
